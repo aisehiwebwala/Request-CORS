83b2340813481be61308c0f12bf5ae57ce371755
aisehiwebwala <116726737+aisehiwebwala@users.noreply.github.com>
2024-02-13 21:51:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use("/api", (req, res) => {
   request(`https://google.rtilinks.com/url?a=+${url}`, function (error, response, body) {
   res.send(body);
 });
+  app.use("/api2", (req, res) => {
+  let url = req.query.url;
+  request(url, function (error, response, body) {
+  res.send(body);
+});
     
 });
 app.get("/", (req, res) => res.send("Hello World!"));
