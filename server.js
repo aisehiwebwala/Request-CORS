1ce9f66be0835bece4901205dde10cdd7445d208
aisehiwebwala <116726737+aisehiwebwala@users.noreply.github.com>
2024-02-13 22:00:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,23 @@ const cors = require("cors");
 const port = 3000;
 const request = require("postman-request");
 app.use(cors());
+
 app.use("/api", (req, res) => {
   let url = req.query.url;
-  request(`https://google.rtilinks.com/url?a=+${url}`, function (error, response, body) {
-  res.send(body);
+  request(
+    `https://google.rtilinks.com/url?a=+${url}`,
+    function (error, response, body) {
+      res.send(body);
+    }
+  );
 });
+
 app.use("/api2", (req, res) => {
   let url = req.query.url;
   request(url, function (error, response, body) {
-  res.send(body);
+    res.send(body);
+  });
 });
-    
-});
-app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/", (req, res) => res.send("Hello Dunia!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
